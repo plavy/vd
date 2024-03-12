da70b0ed9a8aea8861b9fa06da3776d0e4e4a8e6
plavy <tinplavec@gmail.com>
2024-03-12 12:00:52 +0100

    button to select and change color of nodes

--- datasets.js
+++ datasets.js
@@ -30,9 +30,9 @@ nodes_B = []
 nodes_C = []
 
 for(let i = 0; i < N; i++) {
-    nodes_A.push({"data": {"id":"A"+i}})
-    nodes_B.push({"data": {"id":"B"+i}})
-    nodes_C.push({"data": {"id":"C"+i}})
+    nodes_A.push({"data": {"id":"A"+i}, "classes": ['groupA']})
+    nodes_B.push({"data": {"id":"B"+i}, "classes": ['groupB']})
+    nodes_C.push({"data": {"id":"C"+i}, "classes": ['groupC']})
 }
 
 edges = []
@@ -47,6 +47,37 @@ edges.push({"data": {"id": "2:1", "source": "B3", "target": "A7"}})
 
 
 const dataset1 = {
+    "nodes": nodes_A.concat(nodes_B).concat(nodes_C),
+    "edges": edges
+}
+
+// dataset 2
+N = 100
+
+nodes_A = []
+nodes_B = []
+nodes_C = []
+
+for(let i = 0; i < N; i++) {
+    nodes_A.push({"data": {"id":"A"+i}, "classes": ['groupA']})
+    nodes_B.push({"data": {"id":"B"+i}, "classes": ['groupB']})
+    if (i <= 0.8 * N) {
+        nodes_C.push({"data": {"id":"C"+i}, "classes": ['groupC']})
+    }
+}
+
+edges = []
+
+for(let i = 0; i < N; i++) {
+    if (i <= 0.2 * N) {
+        edges.push({"data": {"id": "0:"+i, "source": "A"+i, "target": "B"+i}})
+        edges.push({"data": {"id": "1:"+i, "source": "B"+i, "target": "C"+i}})
+    } else if (i > 0.2 * N && i <= 0.8 * N) {
+        edges.push({"data": {"id": "2:"+i, "source": "B"+i, "target": "C"+(i - 0.2 * N)}})
+    }
+}
+
+const dataset2 = {
     "nodes": nodes_A.concat(nodes_B).concat(nodes_C),
     "edges": edges
 }
