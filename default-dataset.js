191ee339740589c4dafb4d07fd4d036e0f44b041
plavy <tinplavec@gmail.com>
2024-04-29 18:40:15 +0200

    add metanodes

--- default-dataset.js
+++ default-dataset.js
@@ -1,5 +1,4 @@
-// dataset 1
-N = 100
+N = 200
 
 nodes_A = []
 nodes_B = []
@@ -28,42 +27,7 @@ edges.push({"data": {"id": "2:4", "source": "B9", "target": "A11"}})
 edges.push({"data": {"id": "2:5", "source": "B10", "target": "A11"}})
 
 
-const dataset1 = {
-    "name": "Dataset 1",
+const defaultDataset = {
     "nodes": nodes_A.concat(nodes_B).concat(nodes_C),
     "edges": edges
 }
-
-// dataset 2
-N = 100
-
-nodes_A = []
-nodes_B = []
-nodes_C = []
-
-for(let i = 0; i < N; i++) {
-    nodes_A.push({"data": {"id":"A"+i}, "classes": ['group0']})
-    nodes_B.push({"data": {"id":"B"+i}, "classes": ['group1']})
-    if (i <= 0.8 * N) {
-        nodes_C.push({"data": {"id":"C"+i}, "classes": ['group2']})
-    }
-}
-
-edges = []
-
-for(let i = 0; i < N; i++) {
-    if (i <= 0.2 * N) {
-        edges.push({"data": {"id": "0:"+i, "source": "A"+i, "target": "B"+i}})
-        edges.push({"data": {"id": "1:"+i, "source": "B"+i, "target": "C"+i}})
-    } else if (i > 0.2 * N && i <= 0.8 * N) {
-        edges.push({"data": {"id": "2:"+i, "source": "B"+i, "target": "C"+(i - 0.2 * N)}})
-    }
-}
-
-const dataset2 = {
-    "name": "Dataset 2",
-    "nodes": nodes_A.concat(nodes_B).concat(nodes_C),
-    "edges": edges
-}
-
-const datasets = [dataset1, dataset2]
