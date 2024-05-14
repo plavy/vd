296209dcf7f0321fa1a1ddfd678ce85288b695d5
plavy <tinplavec@gmail.com>
2024-05-14 15:33:55 +0200

    add ds5

---
+++ datasets/ds5.py
@@ -0,0 +1,41 @@
+import random
+import csv
+
+file_name = 'ds5.csv'
+
+# number of nodes
+N_A = 1000
+N_B = 4000
+N_C = 6000
+
+# number of ABA and ABB connections
+N_ABA = 100
+N_ABB = 2600
+
+connections = []
+# connections = [
+#     ("A0", "B0", "C0"),
+#     ("A4", "B3", "C1")
+# ]
+
+for i_A in range(N_A):
+    i_B = random.randint(0, N_B - 1)
+    i_C = random.randint(0, N_C - 1)
+    i_C2 = random.randint(0, N_C - 1)
+    connections.append((f'A{i_A}',f'B{i_B}',f'C{i_C}'))
+    connections.append((f'A{i_A}',f'B{i_B}',f'C{i_C2}'))
+
+for i_A in range(N_ABA):
+    i_B = random.randint(0, N_B - 1)
+    i_A2 = random.randint(0, N_A - 1)
+    connections.append((f'A{i_A}',f'B{i_B}',f'A{i_A2}'))
+
+for i_A in range(N_ABA):
+    i_B = random.randint(0, N_B - 1)
+    i_B2 = random.randint(0, N_B - 1)
+    connections.append((f'A{i_A}',f'B{i_B}',f'B{i_B2}'))
+
+with open(file_name, 'w', newline='') as f:
+    writer = csv.writer(f)
+    for c in connections:
+        writer.writerow(c)
