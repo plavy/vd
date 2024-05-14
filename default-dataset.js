d5117027d4a1d5026c0fe8c850689bcfc045dfe1
plavy <tinplavec@gmail.com>
2024-05-14 14:14:28 +0200

    add pie chart, fix heatmap

--- default-dataset.js
+++ default-dataset.js
@@ -1,33 +1,14 @@
 N = 200
 
-nodes_A = []
-nodes_B = []
-nodes_C = []
+triplets = []
 
 for(let i = 0; i < N; i++) {
-    nodes_A.push({"data": {"id":"A"+i}, "classes": ['group0']})
-    nodes_B.push({"data": {"id":"B"+i}, "classes": ['group1']})
-    nodes_C.push({"data": {"id":"C"+i}, "classes": ['group2']})
+    triplets.push("A" + i + "," + "B" + i + "," + "C" + i)
 }
 
-edges = []
+triplets.push("A7,B4,B3")
+triplets.push("A9,B6,B8")
+triplets.push("A11,B9,B10")
+triplets.push("A11,B9,A10")
 
-for(let i = 0; i < N; i++) {
-    edges.push({"data": {"id": "0:"+i, "source": "A"+i, "target": "B"+i}})
-    edges.push({"data": {"id": "1:"+i, "source": "B"+i, "target": "C"+i}})
-}
-
-edges.push({"data": {"id": "2:0", "source": "B4", "target": "A7"}})
-edges.push({"data": {"id": "2:1", "source": "B3", "target": "A7"}})
-
-edges.push({"data": {"id": "2:2", "source": "B6", "target": "A9"}})
-edges.push({"data": {"id": "2:3", "source": "B8", "target": "A9"}})
-
-edges.push({"data": {"id": "2:4", "source": "B9", "target": "A11"}})
-edges.push({"data": {"id": "2:5", "source": "B10", "target": "A11"}})
-
-
-const defaultDataset = {
-    "nodes": nodes_A.concat(nodes_B).concat(nodes_C),
-    "edges": edges
-}
+const defaultDataset = triplets;
